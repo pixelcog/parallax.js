4311696cf0e51f894c509aa282df930d721c22ec
Stefanie Hatcher <stefhatcher@gmail.com>
2015-04-13 17:29:02 -0700

    adding responsive functionality with dynamic aspect ratios

--- parallax.js
+++ parallax.js
@@ -1,8 +1,13 @@
 /*!
- * parallax.js v1.3.1 (http://pixelcog.github.io/parallax.js/)
+ * parallax.js v1.3.2 (http://pixelcog.github.io/parallax.js/)
  * @copyright 2015 PixelCog, Inc.
  * @license MIT (https://github.com/pixelcog/parallax.js/blob/master/LICENSE)
  */
+
+/*! Modernizr 2.8.3 (Custom Build) | MIT & BSD
+ * Build: http://modernizr.com/download/#-mq
+ */
+;window.Modernizr=function(a,b,c){function v(a){i.cssText=a}function w(a,b){return v(prefixes.join(a+";")+(b||""))}function x(a,b){return typeof a===b}function y(a,b){return!!~(""+a).indexOf(b)}function z(a,b,d){for(var e in a){var f=b[a[e]];if(f!==c)return d===!1?a[e]:x(f,"function")?f.bind(d||b):f}return!1}var d="2.8.3",e={},f=b.documentElement,g="modernizr",h=b.createElement(g),i=h.style,j,k={}.toString,l={},m={},n={},o=[],p=o.slice,q,r=function(a,c,d,e){var h,i,j,k,l=b.createElement("div"),m=b.body,n=m||b.createElement("body");if(parseInt(d,10))while(d--)j=b.createElement("div"),j.id=e?e[d]:g+(d+1),l.appendChild(j);return h=["&#173;",'<style id="s',g,'">',a,"</style>"].join(""),l.id=g,(m?l:n).innerHTML+=h,n.appendChild(l),m||(n.style.background="",n.style.overflow="hidden",k=f.style.overflow,f.style.overflow="hidden",f.appendChild(n)),i=c(l,a),m?l.parentNode.removeChild(l):(n.parentNode.removeChild(n),f.style.overflow=k),!!i},s=function(b){var c=a.matchMedia||a.msMatchMedia;if(c)return c(b)&&c(b).matches||!1;var d;return r("@media "+b+" { #"+g+" { position: absolute; } }",function(b){d=(a.getComputedStyle?getComputedStyle(b,null):b.currentStyle)["position"]=="absolute"}),d},t={}.hasOwnProperty,u;!x(t,"undefined")&&!x(t.call,"undefined")?u=function(a,b){return t.call(a,b)}:u=function(a,b){return b in a&&x(a.constructor.prototype[b],"undefined")},Function.prototype.bind||(Function.prototype.bind=function(b){var c=this;if(typeof c!="function")throw new TypeError;var d=p.call(arguments,1),e=function(){if(this instanceof e){var a=function(){};a.prototype=c.prototype;var f=new a,g=c.apply(f,d.concat(p.call(arguments)));return Object(g)===g?g:f}return c.apply(b,d.concat(p.call(arguments)))};return e});for(var A in l)u(l,A)&&(q=A.toLowerCase(),e[q]=l[A](),o.push((e[q]?"":"no-")+q));return e.addTest=function(a,b){if(typeof a=="object")for(var d in a)u(a,d)&&e.addTest(d,a[d]);else{a=a.toLowerCase();if(e[a]!==c)return e;b=typeof b=="function"?b():b,typeof enableClasses!="undefined"&&enableClasses&&(f.className+=" "+(b?"":"no-")+a),e[a]=b}return e},v(""),h=j=null,e._version=d,e.mq=s,e.testStyles=r,e}(this,this.document);
 
 ;(function ( $, window, document, undefined ) {
 
@@ -50,6 +55,15 @@
 
     if (!this.imageSrc && this.$element.is('img')) {
       this.imageSrc = this.$element.attr('src');
+    }
+
+    if (this.responsive) {
+      this.imageSources = this.$element.find('span');
+      this.loadedImages = {};
+
+      if (!this.imageSources.length) {
+        this.responsive = false;
+      }
     }
 
     var positions = (this.position + '').toLowerCase().match(/\S+/g) || [];
@@ -131,19 +145,21 @@
         self.naturalWidth  = this.naturalWidth  || this.width  || 1;
       }
       self.aspectRatio = self.naturalWidth / self.naturalHeight;
-
       Parallax.isSetup || Parallax.setup();
       Parallax.sliders.push(self);
       Parallax.isFresh = false;
       Parallax.requestRender();
     });
 
-    this.$slider[0].src = this.imageSrc;
+    if (this.responsive) {
+      this.checkResponsiveImages();
+    } else {
+      this.$slider[0].src = this.imageSrc;
+    }
 
     if (this.naturalHeight && this.naturalWidth || this.$slider[0].complete) {
       this.$slider.trigger('load');
     }
-
   };
 
 
@@ -159,6 +175,10 @@
     overScrollFix: false,
 
     refresh: function() {
+      if (this.responsive) {
+        this.checkResponsiveImages();
+      }
+
       this.boxWidth        = this.$element.outerWidth();
       this.boxHeight       = this.$element.outerHeight() + this.bleed * 2;
       this.boxOffsetTop    = this.$element.offset().top - this.bleed;
@@ -240,6 +260,54 @@
         width: this.imageWidth,
         maxWidth: 'none'
       });
+    },
+
+    checkResponsiveImages: function() {
+      if (!this.responsive) {
+        return;
+      }
+
+      var matches = [];
+      for (var i = 0, l = this.imageSources.length; i < l; i++) {
+        var src = this.imageSources[i].getAttribute('data-image-src');
+        var media = this.imageSources[i].getAttribute('data-media-query');
+
+        if (src && (!media || Modernizr.mq(media))) {
+          matches.push(src);
+        }
+      }
+
+      if (matches.length) {
+        var src = matches.pop();
+        var exp = new RegExp(src.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'));
+
+        if (!exp.test(this.$slider.attr('src'))) {
+          var alreadyLoaded = this.loadedImages[src];
+
+          if (typeof alreadyLoaded !== 'undefined') {
+            this.setResponsiveImage(alreadyLoaded);
+          } else {
+            var img = new Image();
+
+            img.onload = function() {
+              this.loadedImages[src] = img;
+              this.setResponsiveImage(img);
+            }.bind(this);
+
+            img.src = src;
+          }
+        }
+      }
+    },
+
+    setResponsiveImage: function(img) {
+      this.naturalWidth = img.width;
+      this.naturalHeight = img.height;
+
+      this.imageSrc = img.src;
+      this.$slider[0].src = this.imageSrc;
+
+      this.aspectRatio = this.naturalWidth / this.naturalHeight;
     }
   });
 
