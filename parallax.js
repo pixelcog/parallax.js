6c87bb8931cf4e73254db0b8f564ffd832da08e6
Mike Greiling <mike@pixelcog.com>
2016-01-29 20:42:07 -0600

    happy 2016!

--- parallax.js
+++ parallax.js
@@ -1,6 +1,6 @@
 /*!
  * parallax.js v1.4.1 (http://pixelcog.github.io/parallax.js/)
- * @copyright 2015 PixelCog, Inc.
+ * @copyright 2016 PixelCog, Inc.
  * @license MIT (https://github.com/pixelcog/parallax.js/blob/master/LICENSE)
  */
 
