e5f763b5d1b4060524909a1e691f613b5f9bb56c
Mike Greiling <mike@pixelcog.com>
2016-01-29 20:40:49 -0600

    fix version number; should be 1.4.1

--- parallax.js
+++ parallax.js
@@ -1,5 +1,5 @@
 /*!
- * parallax.js v1.3.1 (http://pixelcog.github.io/parallax.js/)
+ * parallax.js v1.4.1 (http://pixelcog.github.io/parallax.js/)
  * @copyright 2015 PixelCog, Inc.
  * @license MIT (https://github.com/pixelcog/parallax.js/blob/master/LICENSE)
  */
