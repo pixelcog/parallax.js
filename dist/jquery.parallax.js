eb07f0dd2d1ad210a26682e16fffa15d0cc89cab
wstoettinger <w.stoettinger@gmx.at>
2017-10-12 19:24:58 +0200

    cleaned legacy files

--- dist/jquery.parallax.js
+++ dist/jquery.parallax.js
@@ -60,7 +60,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "495fdc3351b969980db3"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "0e92d0fb35d46808f1e2"; // eslint-disable-line no-unused-vars
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
