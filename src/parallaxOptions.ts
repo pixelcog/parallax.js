990e6bd9bd2b4aa1b6e6e7b6c0591c77deeb39ce
Raul Hidalgo Caballero <raul.hidalgo.sopa@gmail.com>
2017-10-12 22:46:14 +0200

    Defined type of properties of parallaxOptions

--- src/parallaxOptions.ts
+++ src/parallaxOptions.ts
@@ -1,21 +1,23 @@
 export class ParallaxOptions{
-  public src: null;
-  public speed= 0.2;
-  public bleed= 0;
-  public zIndex= -100;
-  public posX= "center";
-  public posY= "center";
-  public overScrollFix= false;
-  public excludeAgents= /(iPod|iPhone|iPad|Android)/;
-  public aspectRatio= null;
+  public src: string | null = null;
+  public speed: number = 0.2;
+  public bleed: number = 0;
+  public zIndex: number = -100;
+  public posX: string = "center";
+  public posY: string = "center";
+  public overScrollFix: boolean = false;
+  public excludeAgents: RegExp = /(iPod|iPhone|iPad|Android)/;
+  public aspectRatio: number | null = null;
 
   // jquery selectors
-  public sliderSelector=">.parallax-slider";
-  public mirrorSelector= "body";
+  public sliderSelector: string = ">.parallax-slider";
+  public mirrorSelector: string = "body";
 
   // callback functions:
-  public afterRefresh= null;
-  public afterRender= null;
-  public afterSetup= null;
-  public afterDestroy= null;
+  //TODO any is type Parallax
+  public afterRefresh: ((any) => void) | null = null;
+  public afterRender: ((any) => void) | null = null;
+  public afterSetup: ((any) => void) | null = null;
+  public afterDestroy: ((any) => void) | null = null;
 }
+
