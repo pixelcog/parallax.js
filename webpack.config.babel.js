f4b39cef40464fc08b16adc955f76cb83d885737
Raul Hidalgo Caballero <raul.hidalgo.sopa@gmail.com>
2017-10-13 07:29:10 +0200

    webpack updated

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -4,7 +4,7 @@ import path from 'path';
 // noinspection JSUnresolvedFunction, JSUnusedGlobalSymbols
 export default {
   entry: {
-    'jquery.parallax': './src/jquery.parallax.js',
+    'jquery.parallax': './src/main.js',
   },
   externals: {
     // don't bundle jquery
